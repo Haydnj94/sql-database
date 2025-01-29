0a3affed0a73b9b90366a5e3a2b5274b15daabe6
Haydn Jones <haydndj@gmail.com>
2025-01-29 17:59:44 +0100

    final update of the day

--- SQL files/Hypothesis_1.sql
+++ SQL files/Hypothesis_1.sql
@@ -26,19 +26,21 @@ group by neighbourhood
 order by number_of_listings;
 
 select 
-       case
-           when listing_counts >= 1000 then 'Very High'
-           when listing_counts >= 500 then 'High'
-           when listing_counts >= 100 then 'Medium'
-           when listing_counts >= 50 then 'Low'
-           else 'Very Low'
-       end as neighbourhood_popularity,
-       sum(listing_counts) as total_listings,
-       concat('$', round(avg(avg_price), 2)) as average_price_per_night
+       neighbourhood_popularity,
+       concat('$', round(avg(overall_avg_price), 2)) as average_price_per_night
 from (
-    select neighbourhood, count(neighbourhood) as listing_counts, avg(price) as avg_price
+    select neighbourhood, 
+           count(neighbourhood) as number_of_listings, 
+           avg(price) as overall_avg_price,
+           case
+               when count(neighbourhood) >= 1000 then 'Very High'
+               when count(neighbourhood) >= 500 then 'High'
+               when count(neighbourhood) >= 100 then 'Medium'
+               when count(neighbourhood) >= 50 then 'Low'
+               else 'Very Low'
+           end as neighbourhood_popularity
     from airbnb
     group by neighbourhood
 ) as subquery
 group by neighbourhood_popularity
-order by total_listings desc;
+order by average_price_per_night desc;
